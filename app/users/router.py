c590ce442594dd40c8e20cc8489e2450e6887f52
vsmrnw <vsevolod.smrnv@gmail.com>
2024-04-03 22:53:20 +0300

    register logout login endpoints

--- app/users/router.py
+++ app/users/router.py
@@ -1,9 +1,13 @@
-from fastapi import APIRouter, HTTPException, status, Response
+from fastapi import APIRouter, Response, Depends
 
 from app.users.auth import get_password_hash, authenticate_user, \
     create_access_token
+from app.users.dependencies import get_current_user
+from app.users.models import Users
 from app.users.repository import UsersRepo
 from app.users.schemas import SUserAuth
+from app.exceptions import UserAlreadyExistsException, \
+    IncorrectEmailOrPasswordException
 
 router = APIRouter(
     prefix="/auth",
@@ -15,7 +19,7 @@ router = APIRouter(
 async def register_user(user_data: SUserAuth):
     existing_user = await UsersRepo.find_one_or_none(email=user_data.email)
     if existing_user:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT)
+        raise UserAlreadyExistsException
     hashed_password = get_password_hash(user_data.password)
     await UsersRepo.add(email=user_data.email, hashed_password=hashed_password)
     return {"message": "user created successfully"}
@@ -25,8 +29,19 @@ async def register_user(user_data: SUserAuth):
 async def login_user(response: Response, user_data: SUserAuth):
     user = await authenticate_user(user_data.email, user_data.password)
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise IncorrectEmailOrPasswordException
     print(user)
-    access_token = create_access_token({"sub": user})
+    access_token = create_access_token({"sub": str(user)})
     response.set_cookie("booking_access_token", access_token, httponly=True)
     return access_token
+
+
+@router.post("/logout")
+async def logout_user(response: Response):
+    response.delete_cookie("booking_access_token")
+    return {"message": "successfully logged out "}
+
+
+@router.get("/me")
+async def read_users_me(current_user: Users = Depends(get_current_user)):
+    return current_user
