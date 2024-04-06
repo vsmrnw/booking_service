3906809c837bcaf1822f250c52ef199524df02f5
vsmrnw <vsevolod.smrnv@gmail.com>
2024-04-06 20:29:51 +0300

    + delete method & merge find_by_id to find_one_or_none

--- app/repository/base.py
+++ app/repository/base.py
@@ -1,17 +1,10 @@
-from sqlalchemy import select, insert
+from sqlalchemy import select, insert, delete
 
 from app.database import async_session_maker
 
 
 class BaseRepo:
     model = None
-
-    @classmethod
-    async def find_by_id(cls, model_id: int):
-        async with async_session_maker() as session:
-            query = select(cls.model).filter_by(id=model_id)
-            result = await session.execute(query)
-            return result.scalar_one_or_none()
 
     @classmethod
     async def find_one_or_none(cls, **filter_by):
@@ -31,5 +24,12 @@ class BaseRepo:
     async def add(cls, **data):
         async with async_session_maker() as session:
             query = insert(cls.model).values(**data)
+            await session.execute(query)
+            await session.commit()
+
+    @classmethod
+    async def delete(cls, **filer_by):
+        async with async_session_maker() as session:
+            query = delete(cls.model).filter_by(**filer_by)
             await session.execute(query)
             await session.commit()
