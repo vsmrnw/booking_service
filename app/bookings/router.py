38dcd4702694bfba2affd6591a4d163ff56dcfef
vsmrnw <vsevolod.smrnv@gmail.com>
2024-04-06 20:18:20 +0300

    add delete methods & schemas and fix logic

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,6 +1,11 @@
+from datetime import date
+
 from fastapi import APIRouter, Depends
+from pydantic import parse_obj_as
+
 from app.bookings.repository import BookingRepo
 from app.bookings.schemas import SBooking
+from app.exceptions import RoomCannotBeBooked
 from app.users.dependencies import get_current_user
 from app.users.models import Users
 
@@ -14,3 +19,27 @@ router = APIRouter(
 async def get_bookings(user: Users = Depends(get_current_user)) -> list[
     SBooking]:
     return await BookingRepo.find_all(user_id=user.id)
+
+
+@router.post("")
+async def add_booking(
+        room_id: int, date_from: date, date_to: date,
+        user: Users = Depends(get_current_user),
+):
+    booking = await BookingRepo.add(
+        user.id,
+        room_id,
+        date_from,
+        date_to)
+    if not booking:
+        raise RoomCannotBeBooked
+    booking = parse_obj_as(SBooking, booking).dict()
+    return booking
+
+
+@router.delete("/{booking_id")
+async def remove_booking(
+        booking_id: int,
+        user: Users = Depends(get_current_user)
+):
+    await BookingRepo.delete(id=booking_id, user_id=user)
--- app/bookings/schemas.py
+++ app/bookings/schemas.py
@@ -1,4 +1,5 @@
 from datetime import date
+from typing import Optional
 
 from pydantic import BaseModel, ConfigDict
 
@@ -13,3 +14,18 @@ class SBooking(BaseModel):
     price: int
     total_cost: int
     total_days: int
+
+
+class SBookingInfo(SBooking):
+    model_config = ConfigDict(from_attributes=True)
+    image_id: int
+    name: str
+    description: Optional[str]
+    services: list[str]
+
+
+class SNewBooking(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    room_id: int
+    date_from: date
+    date_to: date
