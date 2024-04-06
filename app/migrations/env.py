3ab7ffbff620b37b9e54d5b500f9e5c1a6e24621
vsmrnw <vsevolod.smrnv@gmail.com>
2024-04-06 14:40:54 +0300

    refactoring

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -10,10 +10,6 @@ from alembic import context
 sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
 
 from app.database import Base, DATABASE_URL
-from app.hotels.models import Hotels
-from app.rooms.models import Rooms
-from app.users.models import Users
-from app.bookings.models import Bookings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
