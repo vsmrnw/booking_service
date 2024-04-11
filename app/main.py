8a0ea510de21568cee961da96dbde9dbe55cc97b
vsmrnw <vsevolod.smrnv@gmail.com>
2024-04-11 14:15:37 +0300

    add sqlachemy admin panel && made admin models

--- app/main.py
+++ app/main.py
@@ -5,11 +5,17 @@ from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
 
+from sqladmin import Admin, ModelView
+
+from app.admin.views import BookingsAdmin, UsersAdmin, RoomsAdmin, HotelsAdmin
+from app.bookings.models import Bookings
 from app.config import settings
 from app.bookings.router import router as router_bookings
+from app.database import engine
 from app.hotels.router import router as router_hotels
 from app.images.router import router as router_images
 from app.pages.router import router as router_pages
+from app.users.models import Users
 from app.users.router import router_auth, router_users
 
 app = FastAPI()
@@ -45,3 +51,11 @@ async def startup():
     redis = aioredis.from_url(
         f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}")
     FastAPICache.init(RedisBackend(redis), prefix="cache")
+
+
+admin = Admin(app, engine)
+
+admin.add_view(UsersAdmin)
+admin.add_view(HotelsAdmin)
+admin.add_view(BookingsAdmin)
+admin.add_view(RoomsAdmin)
