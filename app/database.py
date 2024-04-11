569565e041422ec677af8dcc6ec7542c06349e76
vsmrnw <vsevolod.smrnv@gmail.com>
2024-04-11 16:57:35 +0300

    change env params and fix session maker for sqlalchemy v2

--- app/database.py
+++ app/database.py
@@ -1,14 +1,20 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy import NullPool
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, \
+    async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
 from app.config import settings
 
-DATABASE_URL = (f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASS}@"
-                f"{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}")
+if settings.MODE == "TEST":
+    DATABASE_URL = settings.TEST_DATABASE_URL
+    DATABASE_PARAMS = {"poolclass": NullPool}
+else:
+    DATABASE_URL = settings.DATABASE_URL
+    DATABASE_PARAMS = {}
 
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(DATABASE_URL, **DATABASE_PARAMS)
 
-async_session_maker = sessionmaker(engine, class_=AsyncSession,
-                                   expire_on_commit=False)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
 class Base(DeclarativeBase):
