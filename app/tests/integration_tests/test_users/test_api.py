611fe74b94d637f2b4add5622c1082c32cb40cd6
vsmrnw <vsevolod.smrnv@gmail.com>
2024-04-13 00:20:35 +0300

    change tests params

---
+++ app/tests/integration_tests/test_users/test_api.py
@@ -0,0 +1,29 @@
+from httpx import AsyncClient
+import pytest
+
+
+@pytest.mark.parametrize("email,password,status_code", [
+    ("new@user.com", "test", 201),
+    ("new@user.com", "test1", 409),
+    ("testsssss", "test0", 422),
+])
+async def test_register_user(email, password, status_code, ac: AsyncClient):
+    response = await ac.post("/auth/register", json={
+        "email": email,
+        "password": password,
+    })
+    assert response.status_code == status_code
+
+
+@pytest.mark.parametrize("email,password,status_code", [
+    ("test@test.com", "test", 200),
+    ("test@example.com", "test", 200),
+    ("wrongesadmail@example.com", "test", 401),
+])
+async def test_login_user(email, password, status_code, ac: AsyncClient):
+    response = await ac.post("/auth/login", json={
+        "email": email,
+        "password": password,
+    })
+
+    assert response.status_code == status_code
---
+++ app/tests/unit_tests/test_users/test_repository.py
@@ -0,0 +1,18 @@
+import pytest
+
+from app.users.repository import UsersRepo
+
+
+@pytest.mark.parametrize("email,is_exists", [
+    ("test@test.com", True),
+    ("test@example.com", True),
+    (".....", False)
+])
+async def test_find_user_by_id(email, is_exists):
+    user = await UsersRepo.find_one_or_none(email=email)
+
+    if is_exists:
+        assert user
+        assert user["email"] == email
+    else:
+        assert not user
